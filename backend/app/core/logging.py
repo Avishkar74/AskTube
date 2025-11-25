ae05669b663f6d70f75b3a9010fe6ed8a638f715
Avishkar74 <avishkarc600@gmail.com>
2025-11-26 00:59:25 +0530

    chore: remove files ignored by new .gitignore

--- backend/app/core/logging.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-import sys
-
-# Basic Loguru setup; can be extended to JSON/structured logs as needed
-logger.remove()
-logger.add(sys.stdout, level="INFO", backtrace=False, diagnose=False)
-
-__all__ = ["logger"]
